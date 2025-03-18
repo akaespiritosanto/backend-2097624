6e7c6b36980856562bd07b38a8f31980e9e35dbd
akaespiritosanto <2097624@student.uma.pt>
2025-03-18 18:47:59 +0000

    dsgsdf
    dfg

---
+++ aula6/client.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+
+axios.get('http://localhost:3000/users')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+  });
+
+axios.get('/user', {
+    params: {
+      ID: 12345
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+  });
+
+axios.post('/user', {
+  firstName: 'Fred',
+  lastName: 'Flintstone'
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+});
+
+async function getUser() {
+  try {
+    const response = await axios.get('/user?ID=12345');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
--- aula6/server.js
+++ aula6/server.js
@@ -1,32 +1,84 @@
-const express = require('express');
-const fs = require('fs');
-const app = express();
-const port = 3000;
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 3000
 
 app.use(express.json());
 
-var fileContent = fs.readFileSync("persons.json");
-var dataObj = JSON.parse(fileContent);
+var fileContent = fs.readFileSync("persons.json")
+var dataObject = JSON.parse(fileContent);
+
+
+//endpoint para obter a lista de pessoas
+app.get('/users', (req, res) => {
+  res.send(dataObject);
+})
+
+
+//function readFileSync(path){
+  //var fileContent = fs.readFileSync(path);
+  //return fileContent;
+//}
+
+
+app.get('/users/:id', (req, res) => {
+  var id = req.params.id;
+  var person = null;
+  for (let i = 0; i < dataObject.data.length; i++) {
+    if(dataObject.data[i].id == id){
+      person = dataObject.data[i];
+    }
+  }
+  if (person){
+    res.send(person);
+  }
+  else{
+    res.send("ID not found!!");
+  }
+});
+
+app.delete('/users/:id', (req, res) =>{
+  var id = req.params.id;
+  const result = dataObject.data.filter((person) => person.id != id)
+  dataObject.data = result;
+  if(result.length == dataObject.data.length){
+    res.status(404).send("Id not found!!")
+  }
+  else{
+    dataObject.data = result;
+    res.send("ID:" + id + "Deleted")
+  }
+
+  res.send(dataObject);
+  
+  
+});
+
 
 
 app.post('/users', (req, res) => {
-    var newPerson = (req.body);
-    dataObj.data.push(newPerson);
-    res.send(req.body);
-  })
-
-app.delete('/users/:id', (req, res) => {
-    var id = req.params.id;
-  })
-
-app.get('/users', (req, res) => {
-    for (let index=0; index < dataObj.data.length; index++){
-        
-    }
-  })
+  var newPerson = req.body;
+  dataObject.data.push(newPerson);
+  res.send("New person added!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
+  console.log(`Example app listening on port ${port}`)
+});
 
-// {"id":6,"firstName":"João","lastName":"Silva","gender":"Masculino","age":41,"profession":"Programador"}
+app.put('./users/:id'), (req, res) => {
+
+  //parametro de rota id
+  var id = req.param.id;
+  //detalhes do body
+  var details = req.body;
+
+  details.id = id;
+
+  for (let i = 0; i < dataObject.data.length; i++){
+    if (dataObject.data[i].id == id) {
+        dataObject.data[i].id = details;
+    }
+
+
+  }};
