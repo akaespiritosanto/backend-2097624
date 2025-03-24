22d74a18ceac24facb18ecf527e27a7ef0c11538
akaespiritosanto <2097624@student.uma.pt>
2025-03-24 11:49:31 +0000

    24/03/2025
    gdfds

---
+++ ficha7/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql2');
+const port = 3000
+
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'password',
+    database : "ficha7-ctesp"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("Connected!")
+});
+
+app.get('/users', (req, res) => {
+    connection.query("SELECT * FROM users", function(err, results, fields) {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Erro ao buscar user");
+        }
+        res.json(results);
+    });
+});
+
+app.post("/users", (req, res) =>{
+    connection.query("SELECTED * FORM users", function(err, results, fields){
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Erro ao")
+
+            
+        }
+    })
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
