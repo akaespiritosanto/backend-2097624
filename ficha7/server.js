d4ef8ca0f680652d1a1c8ec04d4c09ff408ff50a
akaespiritosanto <2097624@student.uma.pt>
2025-03-25 18:34:04 +0000

    25/03/2025

--- ficha7/server.js
+++ ficha7/server.js
@@ -1,22 +1,23 @@
+const { response } = require('express');
 const express = require('express')
 const app = express()
 const mysql = require('mysql2');
 const port = 3000
 
 var connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'password',
-    database : "ficha7-ctesp"
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: "ficha7-ctesp"
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected!")
 });
 
 app.get('/users', (req, res) => {
-    connection.query("SELECT * FROM users", function(err, results, fields) {
+    connection.query("SELECT * FROM users", function (err, results, fields) {
         if (err) {
             console.error(err);
             return res.status(500).send("Erro ao buscar user");
@@ -25,17 +26,17 @@ app.get('/users', (req, res) => {
     });
 });
 
-app.post("/users", (req, res) =>{
-    connection.query("SELECTED * FORM users", function(err, results, fields){
+app.post("/users", (req, res) => {
+    connection.query("SELECT MAX(id) FROM users", function (err, results, fields) {
         if (err) {
             console.error(err);
-            return res.status(500).send("Erro ao")
-
-            
+            return res.status(500).send("Erro ao buscar user");
         }
-    })
+        var max_id = req.json(results);
+        connection.query("INSERT INTO users (id, firstname, lastname, profession, age) VALUES ()")
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 });
