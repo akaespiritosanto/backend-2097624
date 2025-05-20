841bebcd4e0d08a8fe769942ea88970fd388cc8f
akaespiritosanto <2097624@student.uma.pt>
2025-05-20 19:03:55 +0100

    20/05/2025
    vsdvds

--- ficha11/routes/index.js
+++ ficha11/routes/index.js
@@ -1,32 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const indexController = require('../controllers/indexController');
-const auth = require("../utils/auth");
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-
-router.get('/profile', function(req, res, next) {
-  res.render('profile', { token: 'teste', user: 
-    {id:1, email:"email", password:"password"} });
-});
-
-router.get('/login', function(req, res, next) {
-  res.render('login', { message: req.flash('loginMessage') });
-});
-
-router.get('/signup', function(req, res, next) {
-  res.render('signup', { message: req.flash('signupMessage') });
-});
-
-router.get('/profile', auth.authenticateTokenFromSession, function (req, res) {
-  res.render('profile', {
-      user: req.session.user,
-      token: req.session.token
-  });
-});
 
 router.post('/signup', indexController.signup);
 router.post('/login', indexController.login);
--- ficha11/routes/users.js
+++ ficha11/routes/users.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var usersController = require('../controllers/usersController');
 
+const auth = require("../utils/auth");
+
+router.use(auth.authenticateTokenFromHeaders)
+
+
 router.get('/', usersController.getAllUsers);
 router.get('/:id', usersController.getUserById);
 router.post('/', usersController.createUser);
